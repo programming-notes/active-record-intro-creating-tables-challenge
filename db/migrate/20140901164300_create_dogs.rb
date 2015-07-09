6c7d5ab5efb4695d895891ba3af358cd54143079
Torey Hickman <torey@toreyhickman.com>
2015-07-09 12:15:17 -0500

    Update provided migration

--- db/migrate/20140901164300_create_dogs.rb
+++ db/migrate/20140901164300_create_dogs.rb
@@ -1,11 +1,10 @@
 class CreateDogs < ActiveRecord::Migration
   def change
     create_table :dogs do |t|
-      t.string   :name
-      t.string   :license
+      t.string   :name, { null: false, limit: 50 }
+      t.string   :license, { null: false }
       t.integer  :age
       t.integer  :weight
-      t.integer  :owner_id
 
       t.timestamps(null: false)
     end
